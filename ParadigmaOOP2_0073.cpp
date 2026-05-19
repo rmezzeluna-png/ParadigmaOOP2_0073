2b2e5b92f10ce28d0f1884cac85db03fc82dc0eb
rmezzeluna-png <rmezzeluna@gmail.com>
2026-05-19 16:57:24 +0700

    virtual

--- ParadigmaOOP2_0073.cpp
+++ ParadigmaOOP2_0073.cpp
@@ -1,1 +1,11 @@
+//polimorp
 #include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    //      cout<<"Pesan dari seseorang"<<endl;
+    // }
+};
