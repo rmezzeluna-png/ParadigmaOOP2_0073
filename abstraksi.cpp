14443673467398859f7f2c930712a930b3293c97
rmezzeluna-png <rmezzeluna@gmail.com>
2026-05-19 17:16:24 +0700

    public

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 class AbstraksiKlas {
 private: string x, y;
+
+public:
+
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
